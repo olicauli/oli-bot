df48f9d14210239b933f83992288fa5c84c664d5
olicauli <ec2-user@ip-172-31-22-224.us-east-2.compute.internal>
2022-01-11 11:14:56 +0000

    create events, add some commands

---
+++ commands/about.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { version } = require('./config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('replies information about hythlodaeus'),
+    async execute(interaction) {
+        await interaction.reply(`hello! i am a robot created by oli, mostly to manage a to-do list.
+        \nlibrary used: discord.js\ncurrent version: ${version}`);
+    },
+};
