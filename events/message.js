09f6fe83768bc44475e86d8d78d0555700c14da2
olicauli <ec2-user@ip-172-31-22-224.us-east-2.compute.internal>
2022-01-13 18:28:01 +0000

    make message.js test message more clingy

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ module.exports = {
     async execute(message) {
         console.log('in message');
         if (message.author === message.client.user) return;
-        if (message.content.contains("ily hythlodaeus"))
+        if (message.content.contains("ily hythlodaeus please respond"))
         {
             console.log('in message ily');
             await message.react('💞');
